2cc7f569df58c45d81e84addc9f2c7706e730ed0
Kevin Barr <kevin.barr@siemensgamesa.com>
2021-04-28 21:42:29 +0100

    Added caching
    Updated bot to cache on boot.
    Fixed victimisation through cache.

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ const prefix = '!';
 client.on('ready', () => {
     console.log("Logged in as Butch!");
     client.user.setActivity(" Baseili", {type: "WATCHING"});
+
+    client.guilds.fetch('642796636354904074').then((guild) => 
+    {
+        guild.members.fetch().then((members) => 
+        {
+            members.forEach((member) =>
+            {
+                console.log(member.displayName);
+            });
+        });
+    });
 });
 
 client.on('message', message =>
@@ -16,13 +27,15 @@ if (!message.content.startsWith(prefix) || message.author.bot) return;
 const args = message.content.slice(prefix.length).split(' ');
 const command = args.shift().toLowerCase();
 
+
 switch (command)
 {
     case 'ping':
         message.reply("Pong!");
+
         break;
 
-    case 'victimiseAl':
+    case 'victimiseal':
         message.guild.members.fetch('227885873130242049')
         .then(member =>
         {
@@ -36,10 +49,20 @@ switch (command)
             message.reply("Ey up, you're missing the twat you want me to victimise!");
             break;
         }
+        
+        let User = client.users.cache.find(User => User.username.toLowerCase() == args[0]);
+        if (!User)
+        {
+            message.reply("Couldn't find the twat, soz m8.");
+        }
+        else
+        {
+            message.channel.send("Twat, " + "<@" + User.id + ">");
+        }
 
-        var matching_client = client.users.cache.find(user => user.username === args[0]);
+        //var matching_client = client.users.cache.find(user => user.username === args[0]);
 
-        message.reply(matching_client.username);
+        //message.reply(matching_client.username);
         //message.reply("Couldn't find the twat, soz m8.");
         break;
 
@@ -57,4 +80,4 @@ switch (command)
 }
 });
 
-client.login('ODMzMDkyMTg2NDg4MTc2NzAw.YHtTjw.a5U1B_2-6IngVJi4JR3QkSse_hk');
+client.login('ODM3MDU1OTMwNjkyMDA5OTk1.YIm_FQ.ShoMTZHhn-B4-JOKe6ynbV-jtd0');
