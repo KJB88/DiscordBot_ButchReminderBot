c548b477c495d52682f2ce9cdd97705559a4aa64
Kevin Barr <kevin.barr@siemensgamesa.com>
2021-05-10 13:03:08 +0100

    Ready event
    - Updated ready event to fetch target channel for reminder pings.
    - Added thematic responses to !ping (now !wassup).
    - Gave permissions to cache member list.

--- main.js
+++ main.js
@@ -1,83 +1,144 @@
-const Discord = require('discord.js');
+// CORE
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '!';
+const token = "ODM3MDU1OTMwNjkyMDA5OTk1.YIm_FQ.ShoMTZHhn-B4-JOKe6ynbV-jtd0";
 
-client.on('ready', () => {
-    console.log("Logged in as Butch!");
-    client.user.setActivity(" Baseili", {type: "WATCHING"});
+// CORE COMMANDS
+const prefix = "!";
 
-    client.guilds.fetch('642796636354904074').then((guild) => 
-    {
-        guild.members.fetch().then((members) => 
-        {
-            members.forEach((member) =>
-            {
-                console.log(member.displayName);
-            });
-        });
-    });
+// REMINDERS
+var reminderDelay = 24 * 60 * 60 * 1000; // hours in a day * minutes in an hour * seconds in a minute * milliseconds in a second
+var reminderChannel;
+
+// LOGIN
+client.login(token);
+
+// READY EVENT
+client.on("ready", () => {
+  console.log("NOTE: Logged in as Butch! Bot ready.");
+  client.user.setActivity(" Baseili", { type: "WATCHING" });
+
+  console.log("TASK: Fetching member cache...");
+  client.guilds.fetch("642796636354904074").then((guild) => {
+    guild.members.fetch().then((members) => {
+      members.forEach((member) => {
+        console.log(member.displayName);
+      });
+    }).catch(console.error);
+    console.log("COMPLETE: Member caching complete.");
+  }).catch(console.error);
+
+  console.log("TASK: Fetching channel ID for reminder messages...");
+  // 833107676899966996 - The-Turing-Test on Ferrum
+  reminderChannel = client.channels.cache.get('833107676899966996');
+  console.log("COMPLETE: Reminder channel bound to " + reminderChannel.name);
+
+
+  console.log("TASK: Setting interval for message pings.");
+  // REMINDER INTERVAL TIMING
+  setInterval(() => {
+    reminderChannel.send();
+  }, reminderDelay);
 });
 
-client.on('message', message =>
-{
+// MESSAGE EVENT
+client.on("message", (message) => {
+  // Check for prefix and self
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    // Check for prefix and self
-if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
 
-const args = message.content.slice(prefix.length).split(' ');
-const command = args.shift().toLowerCase();
+  switch (command) {
+    case "wassup":
+      switch (getRandomInt(1, 4)) {
+        case 1:
+          message.reply("Orite blud!");
+          break;
 
+        case 2:
+          message.reply("Ere lad!");
+          break;
 
-switch (command)
-{
-    case 'ping':
-        message.reply("Pong!");
+        case 3:
+          message.reply("Wassup m8!");
+          break;
 
+        case 4:
+          message.reply("Braaaaaaaaaap!");
+          break;
+      }
+      break;
+
+    // Set the timer
+    case "setTimer".toLowerCase():
+      // setTimer reminderName number type
+      // eg setTimer remindAl 1 hour
+      break;
+
+    case "getReminder".toLowerCase():
+      // getReminder reminderName
+      // Displays class values
+      // eg Reminder Name: annoyAlex, Delay: 24 hours, Message: REMINDER FOR ALEX
+      break;
+
+    case "addGame".toLowerCase():
+      break;
+
+    case "victimiseAl".toLowerCase():
+      message.guild.members.fetch("227885873130242049").then((member) => {
+        message.channel.send("Twat, " + "<@" + member.id + ">");
+      });
+      break;
+
+    case "victimise":
+      if (args.length == 0) {
+        message.reply(
+          "Ey up, you're missing the twat you want me to victimise!"
+        );
         break;
+      }
 
-    case 'victimiseal':
-        message.guild.members.fetch('227885873130242049')
-        .then(member =>
-        {
-            message.channel.send("Twat, " + "<@" + member.id + ">");
-        });
-        break;
+      let User = client.users.cache.find(
+        (User) => User.username.toLowerCase() == args[0]
+      );
+      if (!User) {
+        message.reply("Couldn't find the twat, soz m8.");
+      } else {
+        message.channel.send("Twat, " + "<@" + User.id + ">");
+      }
 
-    case 'victimise':
-        if (args.length == 0)
-        {
-            message.reply("Ey up, you're missing the twat you want me to victimise!");
-            break;
-        }
-        
-        let User = client.users.cache.find(User => User.username.toLowerCase() == args[0]);
-        if (!User)
-        {
-            message.reply("Couldn't find the twat, soz m8.");
-        }
-        else
-        {
-            message.channel.send("Twat, " + "<@" + User.id + ">");
-        }
+      //var matching_client = client.users.cache.find(user => user.username === args[0]);
 
-        //var matching_client = client.users.cache.find(user => user.username === args[0]);
+      //message.reply(matching_client.username);
+      //message.reply("Couldn't find the twat, soz m8.");
+      break;
 
-        //message.reply(matching_client.username);
-        //message.reply("Couldn't find the twat, soz m8.");
-        break;
+    case "remind":
+      break;
 
-    case 'remind':
-        break;
+    case "update":
+      break;
 
-    case 'update':
-        break;
+    case "args":
+      message.reply(args.length.toString());
+      break;
+  }
+});
 
-    case 'args':
-
-        message.reply(args.length.toString());
-        break;
-
+// NON-EVENT FUNCTIONS
+function Remind() {
+  // Spawn new interval-function
+  // Set reminder time
+  // Set reminder info
+  // Execute
 }
-});
 
-client.login('ODM3MDU1OTMwNjkyMDA5OTk1.YIm_FQ.ShoMTZHhn-B4-JOKe6ynbV-jtd0');
+// HELPER FUNCTIONS
+/// Gets a random integer, both min and max are inclusive.
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
